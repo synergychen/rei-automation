c10025f9b294ec94fef599b4bc68cc54336d8747
Chen Huang <alan.tolearn@gmail.com>
2023-03-18 19:29:44 -0400

    Refactor storage function to class

--- storage.js
+++ storage.js
@@ -1,53 +1,58 @@
-/**
- *
- * @param {string} path
- *  Example: "<zipcode>.<bedrooms>.<forSale/sold/rent>"
- * @param {any} value
- *  Example: { average: 100, median: 90 }
- * @returns {any}
- */
-
-function updateLocalREIData(path, value) {
-  let keys = path.split('.')
-  let data = JSON.parse(localStorage.getItem(localStorageKey())) || {}
-  let originalData = data
-
-  for (let i = 0; i < keys.length - 1; i++) {
-    if (data[keys[i]] === undefined) {
-      data[keys[i]] = {}
-    }
-    data = data[keys[i]]
+class Storage {
+  constructor(key = 'rei') {
+    this.key = key
   }
 
-  data[keys[keys.length - 1]] = value
-  localStorage.setItem(localStorageKey(), JSON.stringify(originalData))
-  return originalData
-}
+  /**
+   *
+   * @param {string} path
+   *  Example: "<zipcode>.<bedrooms>.<forSale/sold/rent>"
+   * @param {any} value
+   *  Example: { average: 100, median: 90 }
+   * @returns {any}
+   */
+  set(path, value) {
+    let keys = path.split('.')
+    let data = JSON.parse(localStorage.getItem(this.key)) || {}
+    let originalData = data
 
-/**
- *
- * @param {string} path
- *  Example: "<zipcode>.<bedrooms>.<forSale/sold/rent>"
- * @returns {any}
- */
-function getLocalREIData(path) {
-  let keys = path.split('.')
-  let data = JSON.parse(localStorage.getItem(localStorageKey())) || {}
-
-  for (let i = 0; i < keys.length; i++) {
-    if (data[keys[i]] === undefined) {
-      return undefined
+    for (let i = 0; i < keys.length - 1; i++) {
+      if (data[keys[i]] === undefined) {
+        data[keys[i]] = {}
+      }
+      data = data[keys[i]]
     }
-    data = data[keys[i]]
+
+    data[keys[keys.length - 1]] = value
+    localStorage.setItem(this.key, JSON.stringify(originalData))
+    return originalData
   }
 
-  return data
-}
+  /**
+   *
+   * @param {string} path
+   *  Example: "<zipcode>.<bedrooms>.<forSale/sold/rent>"
+   * @returns {any}
+   */
+  get(path) {
+    let keys = path.split('.')
+    let data = JSON.parse(localStorage.getItem(this.key)) || {}
 
-function resetREIData() {
-  localStorage.setItem(localStorageKey(), JSON.stringify({}))
-}
+    for (let i = 0; i < keys.length; i++) {
+      if (data[keys[i]] === undefined) {
+        return undefined
+      }
+      data = data[keys[i]]
+    }
 
-function localStorageKey() {
-    return 'rei'
+    return data
+  }
+
+  get root() {
+    return JSON.parse(localStorage.getItem(this.key))
+  }
+
+  reset() {
+    localStorage.setItem(this.key, JSON.stringify({}))
+  }
 }
